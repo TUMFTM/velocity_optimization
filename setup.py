eddf6cdeff6285e268476561f282189c9bc37b75
Thomas Herrmann <herrmann@ftm.mw.tum.de>
2020-06-25 17:22:09 +0200

    gitignore build/ and dist/

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("Readme.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="velocity-planner-mpSQP-Thomas-Herrmann", # Replace with your own username
+    name="velocity-optimization-mpSQP",
     version="0.1",
     author="Thomas Herrmann",
     author_email="thomas.herrmann@tum.de",
@@ -13,6 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="",
     packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy>=1.18.1",
+        "osqp==0.6.1",
+        "sympy>=1.6"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
