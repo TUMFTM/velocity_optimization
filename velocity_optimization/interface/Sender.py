55b446454d5e05ded098f21512bc4c6bfc877c1f
Thomas Herrmann <herrmann@ftm.mw.tum.de>
2021-02-16 14:33:34 +0100

    add information to example message to ESIM

--- velocity_optimization/interface/Sender.py
+++ velocity_optimization/interface/Sender.py
@@ -72,16 +72,22 @@ if __name__ == "__main__":
 
     # test interface: start ESIM and v-planner afterwards: a message on the ESIM-side should appear.
     # --- v_ref: 'v' + track-ID
-    phase = 'r'
+    phase = 'i'
     track = 'mnt'
     # --- init.: 'i' + track-ID + number of laps + initial state x0
     laps = 12
-    x0 = np.array([1, 0, 0, 0.5, 35, 35, 35, 35, 35])
+    x0 = np.array([1, 0, 0, 1.0, 35, 35, 35, 35, 35])
+    # --- re-optim.: 'r' + global coordinate of measurement diff. [m] + measurement diff. [various]
+    s_meas = 70
+    # s [m], v[m/s], t[s], soc_batt [], temp_batt, temp_mach, temp_inv, temp_cool_mi, temp_cool_b [°C]
+    meas_diff = np.array([0, 0, 0, 0, 0.5, 30, 0, 0, 0])
 
     zs_data = dict()
     zs_data['phase'] = phase
     zs_data['track'] = track
     zs_data['num_laps'] = laps
     zs_data['x0'] = x0
+    zs_data['s_meas'] = s_meas
+    zs_data['meas_diff'] = meas_diff
 
     zs.send(zs_data)
